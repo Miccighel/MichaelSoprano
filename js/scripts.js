504e82023a84fcc7465ed60b25bb71dd92f56c2f
Miccighel <miccighel@gmail.com>
2019-03-19 12:17:38 +0100

    Contact me section added

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,7 @@ $(document).ready(() => {
     $("#publications-table").DataTable({
         "paging": false,
         "info": false,
+        "responsive": true,
         "columnDefs": [{
             "targets": 5,
             "orderable": false
